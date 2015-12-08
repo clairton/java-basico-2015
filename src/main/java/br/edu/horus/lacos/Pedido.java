416655bfc7ac510aeddef43a05900f368033a47b
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2015-12-08 21:54:56 -0200

    Criado metodo contar

--- src/main/java/br/edu/horus/lacos/Pedido.java
+++ src/main/java/br/edu/horus/lacos/Pedido.java
@@ -2,6 +2,7 @@ package br.edu.horus.lacos;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class Pedido {
 	private Collection<Item> items = new ArrayList<Item>();
@@ -9,8 +10,25 @@ public class Pedido {
 	public void adicionar(Item item) {
 		items.add(item);
 	}
+	
+	public void remover(Item item) {
+		items.remove(item);
+	}
 
 	public Collection<Item> getItems() {
-		return items;
+		return Collections.unmodifiableCollection(items);
+	}
+
+	public Double totalizar(){
+		/*Double total = 0d;
+		for (Item item : items) {
+			total += item.getValor();
+		}		
+		return total;*/
+		return items.stream().mapToDouble(i -> i.getValor()).sum();
+	}
+	
+	public Integer contar(){
+		return items.size();
 	}
 }
--- src/test/java/br/edu/horus/lacos/PedidoTest.java
+++ src/test/java/br/edu/horus/lacos/PedidoTest.java
@@ -12,7 +12,16 @@ public class PedidoTest {
 		Pedido pedido = new Pedido();
 		Item item = new Item("Laranja", 0.50);
 		pedido.adicionar(item);
-		assertEquals(1, pedido.getItems().size());
+		assertEquals(Integer.valueOf(1), pedido.contar());
+	}
+	
+	@Test
+	public void testRemoverItem(){
+		Pedido pedido = new Pedido();
+		Item item = new Item("Bergamota", 0.80);
+		pedido.adicionar(item);
+		pedido.remover(item);
+		assertEquals(Integer.valueOf(0), pedido.contar());
 	}
 
 }
