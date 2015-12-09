5baa1e9c3eb2caf8d28ffc27a475cfcbfe26b928
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2015-12-09 20:01:48 -0200

    Adicionado teste ordenação Itens por nome

--- src/main/java/br/edu/horus/lacos/Pedido.java
+++ src/main/java/br/edu/horus/lacos/Pedido.java
@@ -1,14 +1,17 @@
 package br.edu.horus.lacos;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class Pedido {
 	private Collection<Item> items = new ArrayList<Item>();
 
-	public void adicionar(Item item) {
-		items.add(item);
+	public void adicionar(Item... item) {
+		items.addAll(Arrays.asList(item));
 	}
 	
 	public void remover(Item item) {
@@ -18,6 +21,8 @@ public class Pedido {
 	public Collection<Item> getItems() {
 		return Collections.unmodifiableCollection(items);
 	}
+	
+	
 
 	public Double totalizar(){
 		/*Double total = 0d;
@@ -31,4 +36,33 @@ public class Pedido {
 	public Integer contar(){
 		return items.size();
 	}
+
+	public void limpar() {
+		items.clear();
+	}
+
+	public Collection<Item> itemsPorNome() {
+		List<Item> list = (List<Item>) items;
+		Collections.sort(list, new Comparator<Item>() {
+
+			@Override
+			public int compare(Item o1, Item o2) {
+				return o1.getNome().compareTo(o2.getNome());
+			}
+		});
+		return list;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/test/java/br/edu/horus/lacos/PedidoTest.java
+++ src/test/java/br/edu/horus/lacos/PedidoTest.java
@@ -3,25 +3,76 @@ package br.edu.horus.lacos;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Collection;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class PedidoTest {
+	private Pedido pedido = new Pedido();	
+	
+	@Before
+	public void inicio(){
+		pedido.limpar();		
+		Item abacaxi = new Item("Abacaxi", 2.04);
+		Item jabuticaba = new Item("Jabuticaba", 3.19);
+		pedido.adicionar(abacaxi, jabuticaba);
+	}
 
 	@Test
 	public void testAdicionarItem() {
-		Pedido pedido = new Pedido();
 		Item item = new Item("Laranja", 0.50);
 		pedido.adicionar(item);
-		assertEquals(Integer.valueOf(1), pedido.contar());
+		assertEquals(Integer.valueOf(3), pedido.contar());
 	}
 	
 	@Test
 	public void testRemoverItem(){
-		Pedido pedido = new Pedido();
 		Item item = new Item("Bergamota", 0.80);
 		pedido.adicionar(item);
 		pedido.remover(item);
-		assertEquals(Integer.valueOf(0), pedido.contar());
+		assertEquals(Integer.valueOf(2), pedido.contar());
+	}	
+	
+	@Test
+	public void testTotalizar(){
+		assertEquals(Double.valueOf(5.23), pedido.totalizar());
 	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void testNaoPodeAdicionarDiretamente(){
+		pedido.getItems().add(new Item("Batata", 2.09d));
+	}
+	
+	@Test
+	public void testItemPorNome(){
+		String nome = "Abacate";
+		Item abacaxi = new Item(nome, 2.04);
+		pedido.adicionar(abacaxi);
+		Collection<Item> items = pedido.itemsPorNome();
+		assertEquals(nome, items.iterator().next().getNome());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
