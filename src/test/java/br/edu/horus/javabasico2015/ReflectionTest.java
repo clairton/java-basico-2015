417416fb4f857517bdc616f0775a9d0ebb4f5dc6
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2015-12-12 17:11:04 -0200

    Exemplo de set com reflection

--- src/test/java/br/edu/horus/javabasico2015/ReflectionTest.java
+++ src/test/java/br/edu/horus/javabasico2015/ReflectionTest.java
@@ -1,7 +1,10 @@
 package br.edu.horus.javabasico2015;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.HashSet;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -31,8 +34,56 @@ public class ReflectionTest {
 		Object pedido = constructor.newInstance();
 		Class<?>[] parametros = new Class<?>[]{Item.class};
 		Method method = tipo.getMethod("remover", parametros);
+		@SuppressWarnings("unused")
 		Object retorno = method.invoke(pedido, item);
 	}
+	
+	@Test
+	public void testSetPorReflection() throws Exception{
+		//TODO limpar a lista de items a força
+		Field field = pedido.getClass().getDeclaredField("items");
+		field.setAccessible(true);//pq o elemento é privado
+		/*
+		 * cuidado na hora de setar o valor, nesse caso
+		 * pode acontecer um problema pois mudei a estrutura de
+		 * dados de List(ArrayList) para Set(HashSet), o mesmo
+		 * não vai dar erro pois ambos implementam Collection
+		 */
+		Collection<Item> value = new HashSet<>();
+		field.set(pedido, value);
+	}
+	
+	@Test
+	public void testString(){
+		String nome = "abc";
+		foo(nome);
+		System.out.println(nome);
+	}
+	
+	public void foo(String nome){
+		nome = "def";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
