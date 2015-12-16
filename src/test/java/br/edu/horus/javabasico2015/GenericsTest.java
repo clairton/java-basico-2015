0ec7e448937d5d6401ed5b9b710d639a08e09407
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2015-12-16 20:12:02 -0200

    Exemplo erase

---
+++ src/test/java/br/edu/horus/javabasico2015/GenericsTest.java
@@ -0,0 +1,24 @@
+package br.edu.horus.javabasico2015;
+
+public class GenericsTest {
+	class Box<T>{
+		T d;	
+		public Box(T d) {
+			super();
+			this.d = d;
+		}
+		T getDefault(){
+			return d;
+		}
+	}	
+	
+	public void test(){
+		//em tempo de compilação
+		Box<String> b = new Box<String>("xpto");
+		String x = b.getDefault();
+
+		//em tempo de execução
+		Box b2 = new Box("");
+		String x2 = (String) b2.getDefault();
+	}	
+}
