ed143b7e53bfe1ac4c7485f07a7e7a3e0c66c6b1
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2016-05-02 22:35:50 -0300

    Iniciao servlet

--- src/main/java/br/edu/horus/javabasico2015/ServicoDao.java
+++ src/main/java/br/edu/horus/javabasico2015/ServicoDao.java
@@ -1,8 +1,14 @@
 package br.edu.horus.javabasico2015;
 
+import java.util.Collection;
+import java.util.List;
+
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import javax.transaction.Transactional;
 
 @Dependent
@@ -19,5 +25,29 @@ public class ServicoDao {
 		manager.persist(entidade);
 		manager.flush();
 		return entidade;
+	}
+
+	public <T>Collection<T> buscar(Class<T> tipo) {
+		CriteriaBuilder cb = manager.getCriteriaBuilder();
+		CriteriaQuery<T> q = cb.createQuery(tipo);
+		q.from(tipo);
+		TypedQuery<T> query = manager.createQuery(q);
+		List<T> results = query.getResultList();
+		return results;
 	}	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
